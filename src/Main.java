c65eec0423242bc26bb8c8667947d40542758e14
lavinaz <nazgnyy@gmail.com>
2025-11-30 14:30:58 +0300

    function 0 is finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-// Main.java — Students version
 import java.io.*;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 
 public class Main {
     static final int MONTHS = 12;
@@ -9,13 +13,64 @@ public class Main {
     static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
     static String[] months = {"January","February","March","April","May","June",
             "July","August","September","October","November","December"};
+    public static int[][][] profit=new int[12][28][5];
 
 
-    // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
-    public static void loadData() {
+
+
+
+    public static void loadData()  { //dosya okuma func
+        for (int m = 0; m < 12; m++) //her bir ay icin
+        {
+            String fileName = "Data_Files/" + months[m] + ".txt"; //konuma bakiyo (ad değiştirilebilir)
+
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) //okuyo
+            {
+                String line;
+
+                while ((line = br.readLine()) != null) //her bir line icin
+                {
+                    String[] parts = line.split(",");  //dosyalardaki elemanlar virgülle ayrıldığı için virgül
+                    if (parts.length != 3) continue; //hata ariyo
+
+                    int day;
+                    try {
+                        day = Integer.parseInt(parts[0].trim()); //dayi ayırıyo
+                    } catch (Exception e) //hata
+                    {
+                        continue;
+                    }
+
+                    String commodity = parts[1].trim();  //commodityi ayırıyo
+                    int profitValue;
+                    try {
+                        profitValue = Integer.parseInt(parts[2].trim()); //value yu ayırıyo
+                    } catch (Exception e)  //hata
+                     {
+                        continue;
+                    }
+
+                    int cIndex = getCommodityIndex(commodity);//commodityi (normalde string) int e ceviriyo
+                    if (cIndex == -1) continue; //hata
+                    if (day < 1 || day > 28) continue;//hata
+
+                    profit[m][day - 1][cIndex] = profitValue; //Bir ayın, gününün, malın, profit valuesunu eşitliyo
+                }
+
+            } catch (IOException e) {
+
+            }
+        }
+    }
+    private static int getCommodityIndex(String c) {
+        for (int i = 0; i < commodities.length; i++) {
+            if (commodities[i].equals(c)) return i;
+        }
+        return -1;
     }
 
-    // ======== 10 REQUIRED METHODS (Students fill these) ========
+
+
 
     public static String mostProfitableCommodityInMonth(int month) {
         return "DUMMY";
